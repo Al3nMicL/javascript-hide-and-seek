63c2193581c0bc694f8c72115caba7aa441ea5f5
Al3nMicL <https://twitter.com/Al3nMicL>
2019-03-14 22:23:37 -0400

    nestedTarget() test passes

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
 let getFirstSelector = (selector) => {
 	return document.querySelector(selector);
 }
+
+// Task 2 - nestedTarget
+let nestedTarget = () => {
+	let parent = document.querySelector('#nested');
+	let child = parent.querySelector('.target');
+	return child;
+}
