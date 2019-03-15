6d6d342d9edb76cf1e23cbb9daf340208117b480
Al3nMicL <https://twitter.com/Al3nMicL>
2019-03-14 23:23:57 -0400

    deepestChild() test passes

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ let nestedTarget = () => {
 	let child = parent.querySelector('.target');
 	return child;
 }
+
+// Task 3 - deepestChild
+let deepestChild = () => {
+	let grand = document.querySelectorAll('#grand-node');
+	let divs = grand[0].querySelectorAll('div');
+	let divs_len = divs.length;
+	let find_last = divs_len - 1;
+	return divs[find_last];
+}
