54b8227cb3ad7e54d8eb3db7d707d328dcf4d8c7
Al3nMicL <https://twitter.com/Al3nMicL>
2019-03-15 14:46:12 -0400

    increaseRankBy() test passes

--- index.js
+++ index.js
@@ -15,6 +15,31 @@ let deepestChild = () => {
 	let grand = document.querySelectorAll('#grand-node');
 	let divs = grand[0].querySelectorAll('div');
 	let divs_len = divs.length;
+	// the deepest child is the last object 
 	let find_last = divs_len - 1;
 	return divs[find_last];
+}
+
+// Task 4 - increaseRankBy
+let increaseRankBy = (n) => {
+	let rankedLists = document.querySelectorAll('.ranked-list');
+
+	let firstList = rankedLists[0];
+	let secondList = rankedLists[1];
+
+	let firstChildren = firstList.children;
+	let secondChildren = secondList.children;
+	// see -> https://stackoverflow.com/a/222847
+	let listArray1 = [].slice.call(firstChildren);
+	let listArray2 = [].slice.call(secondChildren);
+	// to enable usage of map from converted HTMLCollection
+	listArray1.map(function(x, y) {
+		let newVal = parseInt(x.innerHTML) + n;
+		firstChildren[y].innerHTML = newVal;
+	});
+
+	listArray2.map(function(x,y) {
+		let newVal = parseInt(x.innerHTML) + n;	
+		secondChildren[y].innerHTML = newVal;
+	});
 }
